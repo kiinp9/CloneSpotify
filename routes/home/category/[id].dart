f8a9b7e2dfd7f2ccd40937e07bb842da3745db1b
kiinp9 <vietcuong23122k2@ong23122k2.com>
2025-05-01 13:28:07 +0700

    update api category/[id]

--- routes/home/category/[id].dart
+++ routes/home/category/[id].dart
@@ -2,20 +2,17 @@ import 'dart:io';
 
 import 'package:dart_frog/dart_frog.dart';
 
-import '../../constant/config.message.dart';
-import '../../controllers/music_controller.dart';
-import '../../model/response.dart';
+import '../../../constant/config.message.dart';
+import '../../../controllers/music_controller.dart';
+import '../../../model/response.dart';
 
-Future<Response> onRequest(RequestContext context) async {
+Future<Response> onRequest(RequestContext context, String id) async {
   if (context.request.method.value != 'GET') {
-    return AppResponse().error(
-      HttpStatus.methodNotAllowed,
-      ErrorMessage.MSG_METHOD_NOT_ALLOW,
-    );
+    return AppResponse()
+        .error(HttpStatus.methodNotAllowed, ErrorMessage.MSG_METHOD_NOT_ALLOW);
   }
   final musicController = context.read<MusicController>();
-  final categoryId =
-      int.tryParse(context.request.uri.queryParameters['categoryId'] ?? '');
+  final categoryId = int.tryParse(context.request.uri.pathSegments.last);
   if (categoryId == null) {
     return AppResponse().error(HttpStatus.internalServerError,
         ErrorMessageRoute.PARAMETER_QUERY_ROUTER_NOT_NULL);
